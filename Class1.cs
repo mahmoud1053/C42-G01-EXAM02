843d10e105b88e37522f6b30c4410a549482af1e
mahmo <mahmo@7ODA>
2024-07-29 03:42:58 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+
+    public class Answer
+    {
+        public int AnswerId { get; set; }
+        public string AnswerText { get; set; }
+    }
+
+    public abstract class Question
+    {
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public int Mark { get; set; }
+        public List<Answer> AnswerList { get; set; }
+        public Answer RightAnswer { get; set; }
+    }
+
+    public class TrueFalseQuestion : Question
+    {
+        public TrueFalseQuestion()
+        {
+            AnswerList = new List<Answer>
+        {
+            new Answer { AnswerId = 1, AnswerText = "True" },
+            new Answer { AnswerId = 2, AnswerText = "False" }
+        };
+        }
+    }
+
+    public class MCQQuestion : Question
+    {
+        public MCQQuestion(List<Answer> answers, Answer rightAnswer)
+        {
+            AnswerList = answers;
+            RightAnswer = rightAnswer;
+        }
+    }
+
+    public abstract class Exam
+    {
+        public DateTime TimeOfExam { get; set; }
+        public int NumberOfQuestions { get; set; }
+        public List<Question> Questions { get; set; }
+
+        public abstract void ShowExam();
+    }
+
+    public class FinalExam : Exam
+    {
+        public override void ShowExam()
+        {
+            foreach (var question in Questions)
+            {
+                Console.WriteLine($"{question.Header}: {question.Body}");
+                foreach (var answer in question.AnswerList)
+                {
+                    Console.WriteLine($"- {answer.AnswerText}");
+                }
+            }
+        }
+    }
+
+    public class PracticalExam : Exam
+    {
+        public override void ShowExam()
+        {
+            foreach (var question in Questions)
+            {
+                Console.WriteLine($"{question.Header}: {question.Body}");
+                foreach (var answer in question.AnswerList)
+                {
+                    Console.WriteLine($"- {answer.AnswerText}");
+                }
+            }
+
+            Console.WriteLine("Correct Answers:");
+            foreach (var question in Questions)
+            {
+                Console.WriteLine($"{question.Header}: {question.RightAnswer.AnswerText}");
+            }
+        }
+    }
+
+    public class Subject : ICloneable, IComparable<Subject>
+    {
+        public int SubjectId { get; set; }
+        public string? SubjectName { get; set; }
+        public Exam SubjectExam { get; set; }
+
+        public void CreateExam(Exam exam)
+        {
+            SubjectExam = exam;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        public int CompareTo(Subject other)
+        {
+            return this.SubjectId.CompareTo(other.SubjectId);
+        }
+
+        public override string ToString()
+        {
+            return $"Subject Id: {SubjectId}, Subject Name: {SubjectName}";
+        }
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject math = new Subject
+            {
+                SubjectId = 1,
+                SubjectName = "Mathematics"
+            };
+
+            FinalExam finalExam = new FinalExam
+            {
+                TimeOfExam = DateTime.Now,
+                NumberOfQuestions = 2,
+                Questions = new List<Question>
+            {
+                new TrueFalseQuestion
+                {
+                    Header = "Q1",
+                    Body = "Is 2+2=4?",
+                    Mark = 1,
+                    RightAnswer = new Answer { AnswerId = 1, AnswerText = "True" }
+                },
+                new MCQQuestion(
+                    new List<Answer>
+                    {
+                        new Answer { AnswerId = 1, AnswerText = "2" },
+                        new Answer { AnswerId = 2, AnswerText = "3" },
+                        new Answer { AnswerId = 3, AnswerText = "4" },
+                        new Answer { AnswerId = 4, AnswerText = "5" }
+                    },
+                    new Answer { AnswerId = 3, AnswerText = "4" }
+                )
+                {
+                    Header = "Q2",
+                    Body = "What is 2+2?",
+                    Mark = 1
+                }
+            }
+            };
+
+            math.CreateExam(finalExam);
+
+            math.SubjectExam.ShowExam();
+        }
+    }
+}
